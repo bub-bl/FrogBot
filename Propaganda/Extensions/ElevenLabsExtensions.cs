2d6ffdfb5f73716d435d92d76b41301fcfc3838f
bub.bl <bub.bl@outlook.com>
2024-04-05 11:32:02 +0200

    Configure ElevenLabs service

---
+++ Propaganda/Extensions/ElevenLabsExtensions.cs
@@ -0,0 +1,14 @@
+﻿using ElevenLabs;
+
+namespace Propaganda.Extensions;
+
+public static class ElevenLabsExtensions
+{
+    public static IServiceCollection ConfigureElevenLabs(this IServiceCollection services)
+    {
+        var client = new ElevenLabsClient(ElevenLabsAuthentication.LoadFromEnv());
+        services.AddSingleton(client);
+
+        return services;
+    }
+}
--- Propaganda/Program.cs
+++ Propaganda/Program.cs
@@ -7,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+builder.Services.ConfigureElevenLabs();
 builder.Services.AddPropaganda();
 
 var app = builder.Build();
