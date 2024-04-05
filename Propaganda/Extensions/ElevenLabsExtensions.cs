8117d9999d03a6254b421610a78e0d4a32524b78
bub.bl <bub.bl@outlook.com>
2024-04-05 13:18:57 +0200

    Get key from config instead of env folder

--- Propaganda/Extensions/ElevenLabsExtensions.cs
+++ Propaganda/Extensions/ElevenLabsExtensions.cs
@@ -6,7 +6,12 @@ public static class ElevenLabsExtensions
 {
     public static IServiceCollection ConfigureElevenLabs(this IServiceCollection services)
     {
-        var client = new ElevenLabsClient(ElevenLabsAuthentication.LoadFromEnv());
+        var provider = services.BuildServiceProvider();
+        var configuration = provider.GetRequiredService<IConfiguration>();
+        var apiKey = configuration["ELEVEN_LABS_API_KEY"];
+        
+        var auth = new ElevenLabsAuthentication(apiKey);
+        var client = new ElevenLabsClient(auth);
         services.AddSingleton(client);
 
         return services;
